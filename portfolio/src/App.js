e9001dad8f9d113e4a19e9aad8758944521088a9
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-05 17:35:16 -0800

    Added title and buttons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="nomar-Title" >Nomar Olivas</h1>
+        <button className="software-button">Software Engineer</button>
+        <button className="IT-button">IT Specialist</button>
+        
+        
+        
       </header>
+
+
+
+
+
     </div>
   );
 }
 
 export default App;
+
