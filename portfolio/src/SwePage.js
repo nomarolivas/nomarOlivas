913b53d0e68e8bd361437579333924c3c13ed805
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-13 21:25:48 -0700

    Added css to swe page

--- portfolio/src/SwePage.js
+++ portfolio/src/SwePage.js
@@ -1,12 +1,16 @@
 const Swe = () => {
     return (
-        
-        <div className="sweNav">
-            <h1>Nomar Olivas</h1>
-            <h3>Software Engineer</h3>
-            <h5>About</h5>
-            <h5>Projects</h5>
-            <h5>Resume</h5>
+        <div swePage>
+            <div className="sweNav">
+                <div className="sweLinks">
+
+                    <h1>Nomar Olivas</h1>
+                    <h3>Software Engineer</h3>
+                    <h5>About</h5>
+                    <h5>Projects</h5>
+                    <h5>Resume</h5>
+                </div>
+            </div>
         </div>
 
     );
